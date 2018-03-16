4a2a9eaf901f5fa153a54afce2d3db2acfd39572
Jay Guzman <j.d.guzman.v@gmail.com>
2018-03-16 09:11:10 -0500

    filtered an array of files by extension successfuly

--- learnyounode/lsFilter.js
+++ learnyounode/lsFilter.js
@@ -3,8 +3,13 @@ let endFile = `.${process.argv[3]}`;
 
 let fs = require('fs');
 
-console.log(path, endFile);    
-
-
-
-
+fs.readdir(`${path}`, function (error, data){
+	
+	if (error)console.log(error);
+	
+	let arrFiltered = data.filter(str => str.endsWith(endFile));
+	
+	for (let i = 0; i < arrFiltered.length; i++){
+		console.log(arrFiltered[i]);	
+	};
+});
