d06c73401c0cba1673b5e7981feb7d9d1f298213
Jay Guzman <j.d.guzman.v@gmail.com>
2018-03-18 15:03:33 -0500

    fuction ready to be tested

---
+++ learnyounode/export.js
@@ -0,0 +1,20 @@
+
+module.exports = function ( path, endFile, callback){
+	
+fs.readdir(`${path}`, function (error, data){
+	
+	if (error)console.log(error);
+	
+	let arrFiltered = data.filter(str => str.endsWith(endFile));
+	
+	for (let i = 0; i < arrFiltered.length; i++){
+		console.log(arrFiltered[i]);	
+	};
+});
+
+callback();
+};
+
+ 
+
+
